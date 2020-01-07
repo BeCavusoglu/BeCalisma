ad4e16290fb79937b29afc552b0be226afa0bf3f
BeCavusoglu <bilal.emre.cavusoglu@gmail.com>
2020-01-07 22:55:03 +0300

    4rd check-in

--- PhoneList/ConsoleApp1/Person.cs
+++ PhoneList/ConsoleApp1/Person.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
 {
    public class Person
     {
+        public int identyNo { get; set; }
         public string Name { get; set; }
+        public string LName { get; set; }
     }
 }
