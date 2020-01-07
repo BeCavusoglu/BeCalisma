0f2b022e81df7e01ef5c6af0b71984f845b7be36
BeCavusoglu <bilal.emre.cavusoglu@gmail.com>
2020-01-07 22:52:10 +0300

    3rd checin

--- PhoneList/ConsoleApp1/Program.cs
+++ PhoneList/ConsoleApp1/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
-
+            
         }
     }
 }
